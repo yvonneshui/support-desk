60a4d8d5511ec850dd3585b042decb7029fffdb0
yvonneshui <yvonne.shui126@gmail.com>
2022-03-17 16:40:20 -0400

    backend: crated user model and user signup routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,32 @@
-console.log('server...')
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+const color = require('colors')
+
+const PORT = process.env.PORT || 8000
+
+const {errorHandler} = require ('./middleware/errorMiddleware')
+
+const connectDB = require('./config/db')
+
+// connect database
+connectDB()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+
+
+app.get('/', (req,res) => {
+	res.status(201).json({message:'Hello'})
+})
+
+//Routes
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT,()=>console.log(`server started on port ${PORT}`))
+
